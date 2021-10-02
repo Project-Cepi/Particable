444a312d8dde5a62a6db70b05732e1864337edf4
LeoDog896 <LeoDog896@hotmail.com>
2021-10-02 11:38:13 -0400

    Fix imports

--- common/src/test/kotlin/test.kt
+++ common/src/test/kotlin/test.kt
@@ -1,10 +1,2 @@
-import net.kyori.adventure.audience.Audience
-import world.cepi.particle.Particle
-import world.cepi.particle.ParticleType
-import world.cepi.particle.renderer.Renderer
-import world.cepi.particle.data.Color
-import world.cepi.particle.renderer.shape.SphereRenderer
-import world.cepi.particle.showParticle
-
 fun main() {
 }
